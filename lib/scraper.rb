069abb77c5af987b2cb74550e12e096a685a327a
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-13 11:48:36 +0100

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(html)
+puts doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css(".title-oE5vT4")
+
+courses.each do |course|
+    puts course.text.strip
+end
+
+p doc.css(".headline-26OIBN")[0].name
+
+p doc.css(".headline-26OIBN")[0].attributes
 
 
+# binding.pry
+# 'save'
